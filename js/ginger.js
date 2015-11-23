a00b31ba8bddd52c7b6287360cbbc42a2fe1430a
David Poxon <david.poxon@moinno.com>
2015-11-23 17:21:13 +1000

    Initial website commit

---
+++ js/ginger.js
@@ -0,0 +1,18 @@
+﻿angular.module('ginger', ['ngRoute', 'ngLoadScript', 'gist-embed'])
+
+.config(function ($routeProvider) {
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'js/template/editor.html'
+    })
+        .when('/lexicon', {
+            templateUrl: 'js/template/lexicon.html'
+        })
+        .when('/grammar', {
+            templateUrl: 'js/template/grammar.html'
+        })
+    .otherwise({
+        redirectTo: '/'
+    })
+})
