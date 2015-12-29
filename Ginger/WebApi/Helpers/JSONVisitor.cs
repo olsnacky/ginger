601c588b4d02aeba6e37aec40aa5a1aacd053072
David Poxon <david.poxon@moinno.com>
2015-12-29 19:41:35 +1000

    Progress

--- Ginger/WebApi/Helpers/JSONVisitor.cs
+++ Ginger/WebApi/Helpers/JSONVisitor.cs
@@ -12,7 +12,8 @@ namespace WebApi.Helpers
 {
     enum EdgeRelation
     {
-        AST
+        AST,
+        ControlFlow
     }
 
     enum NodeType
@@ -83,6 +84,19 @@ namespace WebApi.Helpers
             }
 
             _edges.Add(edge);
+        }
+
+        public bool edgeExists(GraphNode source, GraphNode target, EdgeRelation relation)
+        {
+            foreach(GraphEdge edge in _edges)
+            {
+                if (edge.source == source.id && edge.target == target.id && edge.relation == relation)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 
@@ -229,14 +243,29 @@ namespace WebApi.Helpers
         
     }
 
+    public class MappingComparer : IEqualityComparer<Node>
+    {
+        public bool Equals(Node obj1, Node obj2)
+        {
+            return Object.ReferenceEquals(obj1, obj2);
+        }
+
+        public int GetHashCode(Node obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
     class JSONVisitor : SLVisitor
     {
         private const string DEFAULT_ID = "-1";
         private Graph _graph;
         private Stack<GraphNode> _nodeStack;
+        private Dictionary<Node, GraphNode> _mappings;
 
         public JSONVisitor(StatementList graph)
         {
+            _mappings = new Dictionary<Node, GraphNode>(new MappingComparer());
             _nodeStack = new Stack<GraphNode>();
             _graph = new Graph();
             graph.accept(this);
@@ -255,8 +284,10 @@ namespace WebApi.Helpers
         public void visitAssign(Assign a)
         {
             GraphNode assignment = new GraphNode(getNewId(), NodeType.Assignment, "Assign");
+            _mappings.Add(a, assignment);
             _graph.add(assignment);
             addASTEdge(assignment, "");
+            addCFGEdges(a);
 
             visitChildren(assignment, a);
         }
@@ -265,6 +296,7 @@ namespace WebApi.Helpers
         {
             string operation = bo.op == Ginger.GingerToken.Addition ? "+" : "-";
             GraphNode binaryOp = new GraphNode(getNewId(), NodeType.BinaryOperation, $"Binary Operation ({operation})");
+            _mappings.Add(bo, binaryOp);
             _graph.add(binaryOp);
             addASTEdge(binaryOp, "");
 
@@ -274,6 +306,7 @@ namespace WebApi.Helpers
         public void visitBoolean(GingerParser.Boolean b)
         {
             GraphNode boolean = new GraphNode(getNewId(), NodeType.Boolean, "Boolean");
+            _mappings.Add(b, boolean);
             _graph.add(boolean);
             addASTEdge(boolean, "");
         }
@@ -281,8 +314,10 @@ namespace WebApi.Helpers
         public void visitBranch(If b)
         {
             GraphNode branch = new GraphNode(getNewId(), NodeType.Branch, "If");
+            _mappings.Add(b, branch);
             _graph.add(branch);
             addASTEdge(branch, "");
+            addCFGEdges(b);
 
             visitChildren(branch, b);
         }
@@ -290,6 +325,7 @@ namespace WebApi.Helpers
         public void visitDeclaration(Declaration d)
         {
             GraphNode declaration = new GraphNode(getNewId(), NodeType.Declaration, "Declaration");
+            _mappings.Add(d, declaration);
             _graph.add(declaration);
             addASTEdge(declaration, "");
 
@@ -299,6 +335,7 @@ namespace WebApi.Helpers
         public void visitIdentifier(Identifier i)
         {
             GraphNode identifier = new GraphNode(getNewId(), NodeType.Identifier, i.name);
+            _mappings.Add(i, identifier);
             _graph.add(identifier);
             addASTEdge(identifier, "");
         }
@@ -307,6 +344,7 @@ namespace WebApi.Helpers
         {
             string operation = c.op == Ginger.GingerToken.LessThan ? "<" : ">";
             GraphNode inequalityOp = new GraphNode(getNewId(), NodeType.InequalityOperation, $"Inequality Operation ({operation})");
+            _mappings.Add(c, inequalityOp);
             _graph.add(inequalityOp);
             addASTEdge(inequalityOp, "");
 
@@ -316,6 +354,7 @@ namespace WebApi.Helpers
         public void visitInteger(Integer i)
         {
             GraphNode integer = new GraphNode(getNewId(), NodeType.Integer, "Integer");
+            _mappings.Add(i, integer);
             _graph.add(integer);
             addASTEdge(integer, "");
         }
@@ -323,6 +362,7 @@ namespace WebApi.Helpers
         public void visitLiteral<T>(Literal<T> l) where T : Typeable
         {
             GraphNode literal = new GraphNode(getNewId(), NodeType.Literal, ((ILiteral)l.value).value);
+            _mappings.Add(l, literal);
             _graph.add(literal);
             addASTEdge(literal, "");
         }
@@ -330,6 +370,7 @@ namespace WebApi.Helpers
         public void visitStatementList(StatementList sl)
         {
             GraphNode statementList = new GraphNode(getNewId(), NodeType.StatementList, "Statement List");
+            _mappings.Add(sl, statementList);
             _graph.add(statementList);
 
             if (_nodeStack.Count > 0)
@@ -343,8 +384,10 @@ namespace WebApi.Helpers
         public void visitWhile(While w)
         {
             GraphNode whileNode = new GraphNode(getNewId(), NodeType.While, "While");
+            _mappings.Add(w, whileNode);
             _graph.add(whileNode);
             addASTEdge(whileNode, "");
+            addCFGEdges(w);
 
             visitChildren(whileNode, w);
         }
@@ -368,6 +411,49 @@ namespace WebApi.Helpers
         {
             GraphEdge edge = new GraphEdge(_nodeStack.Peek(), node, EdgeRelation.AST, label, false);
             _graph.add(edge);
+        }
+
+        private void addCFGEdge(GraphNode source, GraphNode target)
+        {
+            GraphEdge edge = new GraphEdge(source, target, EdgeRelation.ControlFlow, "CF", true);
+            _graph.add(edge);
+        }
+
+        private void addCFGEdges(Statement statement)
+        {
+            addCFGEdges(statement, true, statement.cfgSuccessors);
+            addCFGEdges(statement, false, statement.cfgPredecessors);
+        }
+
+        private void addCFGEdges(Statement statement, bool source, List<Statement> linkedStatements)
+        {
+            foreach (Statement linkedStatement in linkedStatements)
+            {
+                GraphNode firstNode;
+                GraphNode secondNode;
+
+                if (_mappings.TryGetValue(statement, out firstNode) && _mappings.TryGetValue(linkedStatement, out secondNode))
+                {
+                    GraphNode sourceNode;
+                    GraphNode targetNode;
+
+                    if (source)
+                    {
+                        sourceNode = firstNode;
+                        targetNode = secondNode;
+                    }
+                    else
+                    {
+                        sourceNode = secondNode;
+                        targetNode = firstNode;
+                    }
+
+                    if (!_graph.edgeExists(sourceNode, targetNode, EdgeRelation.ControlFlow))
+                    {
+                        addCFGEdge(sourceNode, targetNode);
+                    }
+                }
+            }
         }
     }
 }
